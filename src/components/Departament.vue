dd5228311ecb943a16156b9cae77e4266c1db280
Luis J. Centeno <luisjcenteno@live.com>
2018-04-01 00:19:31 -0600

    Generate departament partials

--- src/components/Departament.vue
+++ src/components/Departament.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="departament-page">
-    <el-row>
-      <el-col class="page-content" :lg="{span: 12, offset: 6}">
+    <el-row :gutter="10">
+      <el-col class="page-content" :sm="{span: 16, offset: 4}" :md="{span: 12, offset: 6}">
         <div class="header" :style="{ 'background-image': 'url('+ departament.cover +')' }">
           <div class="toolbar">
             <button @click="goTo('/departaments')" class="back icon-btn">
@@ -24,38 +24,16 @@
         <div class="container">
           <el-tabs>
             <el-tab-pane label="Inicio">
-
-              <el-card>
-                <div slot="header">
-                  Información
-                </div>
-                {{ departament.description }}
-              </el-card>
-
-              <h2>Actividades</h2>
-              <el-row>
-                <el-col v-for="activity of departament.activities" :key="activity.id+'activity'" :lg="{span: 12}" :xs="24">
-                  <el-card class="activity-card" :body-style="{ padding: '0px' }">
-                    <img :src="activity.image" :alt="activity.name">
-                    <div style="padding: 14px">
-                      <span class="title">{{ activity.name }}</span>
-                      <p>{{ activity.short_desc }}</p>
-                      <el-button type="text" style="float: right">Ver detalles</el-button>
-                    </div>
-                  </el-card>
-
-                </el-col>
-              </el-row>
-
+              <departament-info-partial :departament="departament"></departament-info-partial>
             </el-tab-pane>
             <el-tab-pane label="Lugares">
               Lugares
             </el-tab-pane>
             <el-tab-pane label="Galería">
-              Lugares
+              Galery
             </el-tab-pane>
             <el-tab-pane label="Comentarios">
-              Lugares
+              Reviews
             </el-tab-pane>
           </el-tabs>
         </div>
@@ -65,7 +43,9 @@
 </template>
 
 <script>
+import DepartamentInfo from './partials/departament-page/DepartamentInfo.vue'
 export default {
+  components: { 'departament-info-partial': DepartamentInfo },
   data() {
     return {
       departament: {
@@ -76,9 +56,34 @@ export default {
           'El departamento de Estelí tiene mucho que ofrecer a sus visitantes. Zona fresca y de importante producción de café, tabaco y hortalizas, se ha desarrollado en las últimas décadas ofertas turísticas que ofrecen la posibilidad de conocer de cerca y desde adentro el proceso de producción de estos distintos rublos, así como su transformación y comercialización. Tanto es así que existen la Ruta del Café y la Ruta del Tabaco',
         activities: [
           {
+            id: 1,
             name: 'Actividad 1',
-            short_desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            image: require('../assets/img/departamentos/Masaya.jpg')
+            short_desc:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            image: require('../assets/img/default.jpg')
+          },
+          {
+            id: 2,
+            name: 'Actividad 2',
+            short_desc:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            image: require('../assets/img/default-2.jpg')
+          }
+        ],
+        news: [
+          {
+            id: 1,
+            title: 'Novedad 1',
+            short_desc:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            image: require('../assets/img/default.jpg')
+          },
+          {
+            id: 2,
+            title: 'Novedad 2',
+            short_desc:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            image: require('../assets/img/default-2.jpg')
           }
         ]
       }
---
+++ src/components/partials/departament-page/DepartamentInfo.vue
@@ -0,0 +1,57 @@
+<template>
+  <div v-if="departament" id="departament-info-partial">
+
+    <el-card>
+      <div slot="header">
+        Información
+      </div>
+      {{ departament.description }}
+    </el-card>
+
+    <!-- ACTIVITIES -->
+
+    <h2>Actividades</h2>
+    <el-row :gutter="10">
+      <el-col v-for="activity of departament.activities" :key="activity.id+'activity'" :sm="{span: 12}" :xs="24">
+        <el-card class="activity-card" :body-style="{ padding: '0px' }">
+          <img :src="activity.image" :alt="activity.name">
+          <div class="body-card">
+            <span class="title">{{ activity.name }}</span>
+            <p>{{ activity.short_desc }}</p>
+            <el-button class="details" type="text">Ver detalles</el-button>
+          </div>
+        </el-card>
+
+      </el-col>
+    </el-row>
+
+    <!-- NEWS -->
+
+    <h2>Novedades</h2>
+    <el-row :gutter="10">
+      <el-col v-for="news_item of departament.news" :key="'news'+news_item.id" :sm="{span: 12}" :xs="24">
+        <el-card class="activity-card" :body-style="{ padding: '0px' }">
+          <img :src="news_item.image" :alt="news_item.title">
+          <div class="body-card">
+            <span class="title">{{ news_item.title }}</span>
+            <p>{{ news_item.short_desc }}</p>
+            <el-button class="details" type="text">Ver más</el-button>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'departament-info-partial',
+  props: {
+    departament: Object
+  }
+}
+</script>
+
+<style>
+
+</style>
